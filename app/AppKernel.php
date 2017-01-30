4062dff46d51209c6ae2e2b3cf5c373ca3860deb
Jeff42 <jeanfrancois.rebaud@yahoo.fr>
2017-01-30 16:04:31 +0100

    installation de FOSUserBundle - Phae 1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new Testmongo\StoreBundle\TestmongoStoreBundle(),
         ];
 
