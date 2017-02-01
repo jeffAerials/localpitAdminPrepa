4346f7907d9977f126cb39a36e2de19f048c2ea4
Jeff42 <jeanfrancois.rebaud@yahoo.fr>
2017-02-01 16:40:17 +0100

    affectation des roles suivant les inscriptions via le formulaire d'enregistrement

---
+++ src/Locass/UserBundle/EventListener/RegistrationListener.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jean-François
+ * Date: 01/02/2017
+ * Time: 15:48
+ */
+
+namespace Locass\UserBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+
+class RegistrationListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return array (
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess'
+        );
+    }
+
+    public function onRegistrationSuccess(FormEvent $event){
+
+
+
+        $user = $event->getForm()->getData();
+        $application = $user->getApplication();
+        $roles = array('ROLE_'.$application);
+
+        $user->setRoles($roles);
+    }
+}
