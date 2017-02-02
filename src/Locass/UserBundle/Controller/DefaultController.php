4bae361a7821d42f8a7081aa575470c633f1a154
Jeff42 <jeanfrancois.rebaud@yahoo.fr>
2017-02-02 09:31:57 +0100

    préparation de la homepage pour la suite de l'inscription

--- src/Locass/UserBundle/Controller/DefaultController.php
+++ src/Locass/UserBundle/Controller/DefaultController.php
@@ -8,6 +8,11 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        // 1 - Redéfinir le role en ROLE_USER ds RegistrationListener.php
+        // 2 - Présenter la suite du formulaire d'inscription en fonction de getApplication UserBundle
+        // 3 - Affecter le ROLE_.. correspondant après event formulaire succes (avant enregistrement update)
+        // 4 - Enregistrer les data dans les entity correspondantes (Salle, Bands, Orga) + mongoDB
+        // 5 - Rediriger vers la route correspondante à la variable application
         return $this->render('LocassUserBundle:Default:index.html.twig');
     }
 }
