3bc65c5f86072e458246017ea6094f682ca14cce
Jeff42 <jeanfrancois.rebaud@yahoo.fr>
2017-03-11 23:31:18 +0100

    Ajout du service de verif si l'email validé de la société principal: checkenablesociety email

--- src/Locass/BandsBundle/Controller/DefaultController.php
+++ src/Locass/BandsBundle/Controller/DefaultController.php
@@ -8,6 +8,16 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        // On récupère le service
+        $isEnableSociety = $this->container->get('locass_user.check_email_society');
+        $user = $this->getUser();
+        $idmongo = $user->getIdmongo();
+        $enable = $isEnableSociety->isEmailsocietyenabled($idmongo);
+
+        if ($enable == true) {
+            throw new \Exception('Votre message a été détecté comme spam !');
+        }
+
         return $this->render('LocassBandsBundle:Default:index.html.twig');
     }
 }
---
+++ src/Locass/UserBundle/Service/checkEmailSociety.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jean-François
+ * Date: 11/03/2017
+ * Time: 14:54
+ */
+
+namespace Locass\UserBundle\Service;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Locass\UserBundle\Document\Contacts;
+use Locass\OrgaBundle\Document\OrgaDocument;
+use Locass\BandsBundle\Document\BandDocument;
+use Locass\SallesBundle\Document\SalleDocument;
+
+
+class checkEmailSociety
+{
+
+    protected $dm;
+
+
+    public function __construct(DocumentManager $documentManager)
+    {
+        $this->dm = $documentManager;
+
+    }
+
+    /**
+     * Vérifie si le contact document possède une société principale validée
+     *
+     * @param string $idmongo
+     * @return bool
+     */
+    public function isEmailsocietyenabled ($idmongo)
+    {
+
+        $contact = $this->dm->getRepository('LocassUserBundle:Contacts')->find($idmongo);
+        $type = $contact->getType();
+        $idprinc = $contact->getIdprinc();
+        if($type == "orga")
+        {
+            $society = $this->dm->getRepository('LocassOrgaBundle:OrgaDocument')->find($idprinc);
+        }
+        elseif ($type == "band")
+        {
+            $society = $this->dm->getRepository('LocassBandsBundle:BandDocument')->find($idprinc);
+        }
+        else
+        {
+            $society = $this->dm->getRepository('LocassSallesBundle:SalleDocument')->find($idprinc);
+        }
+        $enable = $society->getEnable();
+
+        return $enable;
+    }
+}
